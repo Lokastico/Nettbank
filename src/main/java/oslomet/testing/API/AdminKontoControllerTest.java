54ccd778d4e403e9ce7abdc9bc9c832d5cee9240
Lokastico <56114098+Lokastico@users.noreply.github.com>
2021-02-23 11:04:19 +0100

    Importerte de bilbliotekene vi trenger, og lagde en metode i hver testklasse. disse må fylles ut.

--- src/main/java/oslomet/testing/API/AdminKontoControllerTest.java
+++ src/main/java/oslomet/testing/API/AdminKontoControllerTest.java
@@ -1,4 +1,11 @@
 package oslomet.testing.API;
+import org.testng.annotations.Test;
+import static org.junit.Assert.*;
 
 public class AdminKontoControllerTest {
+
+    @Test
+    public void testAdminKontoController(){
+
+    }
 }
--- src/main/java/oslomet/testing/API/BankControllerTest.java
+++ src/main/java/oslomet/testing/API/BankControllerTest.java
@@ -1,4 +1,12 @@
 package oslomet.testing.API;
+import org.testng.annotations.Test;
+import static org.junit.Assert.*;
+
 
 public class BankControllerTest {
+
+    @Test
+    public void testBankController(){
+
+    }
 }
