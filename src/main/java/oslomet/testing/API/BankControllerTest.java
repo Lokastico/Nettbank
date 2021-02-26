2938ae07417780486916f31e158a9d935e20ba94
iselinas <objektgrafikk@live.com>
2021-02-26 14:38:15 +0100

    Flyttet kode fra BankControllerTest (står nå tom) til EnhetstestBankController under test og satt opp mal for hver enhetstest

--- src/main/java/oslomet/testing/API/BankControllerTest.java
+++ src/main/java/oslomet/testing/API/BankControllerTest.java
@@ -1,27 +1,8 @@
 package oslomet.testing.API;
 
-import java.util.List;
-import org.testng.annotations.Test;
-import static org.junit.Assert.*;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-import oslomet.testing.DAL.BankRepository;
-import oslomet.testing.Models.Konto;
-import oslomet.testing.Models.Kunde;
-import oslomet.testing.Models.Transaksjon;
-import oslomet.testing.Sikkerhet.Sikkerhet;
-
-
-
 
 public class BankControllerTest {
 
-    @Test
-    public void testBankController(){
 
-    }
+
 }
--- src/test/java/oslomet/testing/EnhetstestBankController.java
+++ src/test/java/oslomet/testing/EnhetstestBankController.java
@@ -19,25 +19,86 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
+import org.junit.jupiter.api.DisplayName;
+
+
 @RunWith(MockitoJUnitRunner.class)
 public class EnhetstestBankController {
 
     @InjectMocks
-    // denne skal testes
     private BankController bankController;
 
     @Mock
-    // denne skal Mock'es
     private BankRepository repository;
 
     @Mock
-    // denne skal Mock'es
     private Sikkerhet sjekk;
 
+
     @Test
+    @DisplayName("Henter transaksjoner dersom personnr ikke er null")
+    public void testHentTransaksjoner_loggetinn() {
+
+    }
+
+    @Test
+    @DisplayName("Returnerer null hvis personnr er null, transaksjoner")
+    public void testHentTransaksjoner_ikkeloggetinn() {
+
+    }
+
+    @Test
+    @DisplayName("Henter saldi dersom personnr ikke er null")
+    public void testHentSaldi_loggetinn() {
+
+    }
+
+    @Test
+    @DisplayName("Returnerer null hvis personnr er null, saldi")
+    public void testHentSaldi_ikkeloggetinn() {
+
+    }
+
+    @Test
+    @DisplayName("Registrerer betaling dersom personnr ikke er null")
+    public void testRegistrerBetaling_loggetinn() {
+
+    }
+
+    @Test
+    @DisplayName("Returnerer null dersom personnr er null, betalingsregistrering")
+    public void testRegistrerBetaling_ikkeloggetinn() {
+
+    }
+
+    @Test
+    @DisplayName("Henter betaling dersom personnr ikke er null")
+    public void testHentBetaling_loggetinn() {
+
+    }
+
+    @Test
+    @DisplayName("Returnerer null hvis personnr er null, hent betaling")
+    public void testHentBetaling_ikkeloggetinn() {
+
+    }
+
+    @Test
+    @DisplayName("Utfører betaling dersom personnr ikke er null")
+    public void testUtforBetaling() {
+
+    }
+
+    @Test
+    @DisplayName("Returnerer null hvis personnr er null, utfør betaling")
+    public void testUtforBetaling() {
+
+    }
+
+    @Test
+    @DisplayName("Henter kundeinfo dersom kunde er logget inn")
     public void hentKundeInfo_loggetInn() {
 
-        // arrange
         Kunde enKunde = new Kunde("01010110523",
                 "Lene", "Jensen", "Askerveien 22", "3270",
                 "Asker", "22224444", "HeiHei");
@@ -46,29 +107,26 @@ public class EnhetstestBankController {
 
         when(repository.hentKundeInfo(anyString())).thenReturn(enKunde);
 
-        // act
         Kunde resultat = bankController.hentKundeInfo();
 
-        // assert
         assertEquals(enKunde, resultat);
     }
 
     @Test
+    @DisplayName("Returnerer null dersom kunde ikke er logget inn og man prøver å hente kundeinfo")
     public void hentKundeInfo_IkkeloggetInn() {
 
-        // arrange
         when(sjekk.loggetInn()).thenReturn(null);
 
-        //act
         Kunde resultat = bankController.hentKundeInfo();
 
-        // assert
         assertNull(resultat);
     }
 
     @Test
+    @DisplayName("Henter konti dersom kunde er logget inn")
     public void hentKonti_LoggetInn()  {
-        // arrange
+
         List<Konto> konti = new ArrayList<>();
         Konto konto1 = new Konto("105010123456", "01010110523",
                 720, "Lønnskonto", "NOK", null);
@@ -81,23 +139,19 @@ public class EnhetstestBankController {
 
         when(repository.hentKonti(anyString())).thenReturn(konti);
 
-        // act
         List<Konto> resultat = bankController.hentKonti();
 
-        // assert
         assertEquals(konti, resultat);
     }
 
     @Test
+    @DisplayName("Returnerer null hvis kunde ikke er logget inn, konti")
     public void hentKonti_IkkeLoggetInn()  {
-        // arrange
 
         when(sjekk.loggetInn()).thenReturn(null);
 
-        // act
         List<Konto> resultat = bankController.hentKonti();
 
-        // assert
         assertNull(resultat);
     }
 }
