04b41b95956b06831836d3b0eb8d5ae8ce096e15
iselinas <objektgrafikk@live.com>
2021-03-08 17:27:00 +0100

    Fylt ut alle testene i blinde. Tviler på at noen av dem fungerer.

--- src/test/java/oslomet/testing/EnhetstestBankController.java
+++ src/test/java/oslomet/testing/EnhetstestBankController.java
@@ -41,6 +41,19 @@ public class EnhetstestBankController {
     @DisplayName("Henter transaksjoner dersom personnr ikke er null")
     public void testHentTransaksjoner_loggetinn() {
 
+        List<Konto> transaksjon = new ArrayList<>();
+            Konto konto1 = new Konto("123456789","0101010101",2500,
+                                    "Lønnskonto","NOK",null);
+            transaksjon.add(konto1);
+
+            when(sjekk.loggetInn()).thenReturn("123456789");
+
+            when(repository.hentTransaksjoner(anyString())).thenReturn(transaksjon);
+
+            List<Konto> resultat = bankController.hentTransaksjoner('','','');
+
+            assertEquals(transaksjon, resultat);
+
     }
 
     @Test
@@ -49,104 +62,7 @@ public class EnhetstestBankController {
 
         when(sjekk.loggetInn()).thenReturn(null);
 
-        Konto resultat = bankController.hentTransaksjoner(String kontoNr, String fraDato, String tilDato);
-
-        assertNull(resultat);
-    }
-
-    @Test
-    @DisplayName("Henter saldi dersom personnr ikke er null")
-    public void testHentSaldi_loggetinn() {
-
-    }
-
-    @Test
-    @DisplayName("Returnerer null hvis personnr er null, saldi")
-    public void testHentSaldi_ikkeloggetinn() {
-
-        when(sjekk.loggetInn()).thenReturn(null);
-
-        List<Konto> resultat = bankController.hentSaldi();
-
-        assertNull(resultat);
-
-    }
-
-    @Test
-    @DisplayName("Registrerer betaling dersom personnr ikke er null")
-    public void testRegistrerBetaling_loggetinn() {
-
-    }
-
-    @Test
-    @DisplayName("Returnerer null dersom personnr er null, betalingsregistrering")
-    public void testRegistrerBetaling_ikkeloggetinn() {
-
-        when(sjekk.loggetInn()).thenReturn(null);
-
-        String resultat = bankController.registrerBetaling(@RequestBody Transaksjon betaling);
-
-        assertNull(resultat);
-    }
-
-    @Test
-    @DisplayName("Henter betaling dersom personnr ikke er null")
-    public void testHentBetalinger_loggetinn() {
-
-    }
-
-    @Test
-    @DisplayName("Returnerer null hvis personnr er null, hent betaling")
-    public void testHentBetalinger_ikkeloggetinn() {
-
-        when(sjekk.loggetInn()).thenReturn(null);
-
-        List<Transaksjon> resultat = bankController.hentBetalinger();
-
-        assertNull(resultat);
-    }
-
-    @Test
-    @DisplayName("Utfører betaling dersom personnr ikke er null")
-    public void testUtforBetaling() {
-
-    }
-
-    @Test
-    @DisplayName("Returnerer null hvis personnr er null, utfør betaling")
-    public void testUtforBetaling() {
-
-        when(sjekk.loggetInn()).thenReturn(null);
-
-        List<Transaksjon> resultat = bankController.utforBetaling(int txID);
-
-        assertNull(resultat);
-    }
-
-    @Test
-    @DisplayName("Henter kundeinfo dersom kunde er logget inn")
-    public void hentKundeInfo_loggetInn() {
-
-        Kunde enKunde = new Kunde("01010110523",
-                "Lene", "Jensen", "Askerveien 22", "3270",
-                "Asker", "22224444", "HeiHei");
-
-        when(sjekk.loggetInn()).thenReturn("01010110523");
-
-        when(repository.hentKundeInfo(anyString())).thenReturn(enKunde);
-
-        Kunde resultat = bankController.hentKundeInfo();
-
-        assertEquals(enKunde, resultat);
-    }
-
-    @Test
-    @DisplayName("Returnerer null dersom kunde ikke er logget inn og man prøver å hente kundeinfo")
-    public void hentKundeInfo_IkkeloggetInn() {
-
-        when(sjekk.loggetInn()).thenReturn(null);
-
-        Kunde resultat = bankController.hentKundeInfo();
+        Konto resultat = bankController.hentTransaksjoner('String kontoNr', 'String fraDato', 'String tilDato');
 
         assertNull(resultat);
     }
@@ -179,6 +95,152 @@ public class EnhetstestBankController {
         when(sjekk.loggetInn()).thenReturn(null);
 
         List<Konto> resultat = bankController.hentKonti();
+
+        assertNull(resultat);
+    }
+
+    @Test
+    @DisplayName("Henter saldi dersom personnr ikke er null")
+    public void testHentSaldi_loggetinn() {
+
+        List<Konto> saldi = new ArrayList<>();
+        Konto konto1 = new Konto("123456789","0101010101",2500,
+                "Lønnskonto","NOK",null);
+        saldi.add(konto1);
+
+        when(sjekk.loggetInn()).thenReturn("123456789");
+
+        when(repository.hentSaldi(anyString()).thenReturn(saldi));
+
+        List<Konto> resultat = bankController.hentSaldi();
+
+        assertEquals(saldi, resultat);
+    }
+
+    @Test
+    @DisplayName("Returnerer null hvis personnr er null, saldi")
+    public void testHentSaldi_ikkeloggetinn() {
+
+        when(sjekk.loggetInn()).thenReturn(null);
+
+        List<Konto> resultat = bankController.hentSaldi();
+
+        assertNull(resultat);
+    }
+
+    @Test
+    @DisplayName("Registrerer betaling dersom personnr ikke er null")
+    public void testRegistrerBetaling_loggetinn() {
+
+        List<Konto> regbetaling = new ArrayList<>();
+        Konto konto1 = new Konto("123456789","0101010101",2500,
+                "Lønnskonto","NOK",null);
+        regbetaling.add(konto1);
+
+        when(sjekk.loggetInn()).thenReturn("123456789");
+
+        when(repository.registrerBetaling(Transaksjon betaling).thenReturn(regbetaling));
+
+        List<Konto> resultat = bankController.registrerBetaling(Transaksjon betaling);
+
+        assertEquals(regbetaling, resultat);
+
+    }
+
+    @Test
+    @DisplayName("Returnerer null dersom personnr er null, betalingsregistrering")
+    public void testRegistrerBetaling_ikkeloggetinn() {
+
+        when(sjekk.loggetInn()).thenReturn(null);
+
+        String resultat = bankController.registrerBetaling('@RequestBody Transaksjon betaling');
+
+        assertNull(resultat);
+    }
+
+    @Test
+    @DisplayName("Henter betaling dersom personnr ikke er null")
+    public void testHentBetalinger_loggetinn() {
+
+        List<Transaksjon> hentbetaling = new ArrayList<>();
+        Konto konto1 = new Konto("123456789","0101010101",2500,
+                "Lønnskonto","NOK",null);
+        hentbetaling.add(konto1);
+
+        when(sjekk.loggetInn()).thenReturn("123456789");
+
+        when(repository.hentBetalinger(anyString())).thenReturn(hentbetaling);
+
+        List<Transaksjon> resultat = bankController.hentBetalinger();
+
+        assertEquals(hentbetaling, resultat);
+    }
+
+    @Test
+    @DisplayName("Returnerer null hvis personnr er null, hent betaling")
+    public void testHentBetalinger_ikkeloggetinn() {
+
+        when(sjekk.loggetInn()).thenReturn(null);
+
+        List<Transaksjon> resultat = bankController.hentBetalinger();
+
+        assertNull(resultat);
+    }
+
+    @Test
+    @DisplayName("Utfører betaling dersom personnr ikke er null")
+    public void testUtforBetaling_loggetinn() {
+
+        List<Transaksjon> utforbetaling = new ArrayList<>();
+        Konto konto1 = new Konto("123456789","0101010101",2500,
+                "Lønnskonto","NOK",null);
+        utforbetaling.add(konto1);
+
+        when(sjekk.loggetInn()).thenReturn("123456789");
+
+        when(repository.utforBetaling('')).thenReturn(utforbetaling);
+
+        List<Transaksjon> resultat = bankController.utforBetaling('');
+
+        assertEquals(utforbetaling, resultat);
+
+    }
+
+    @Test
+    @DisplayName("Returnerer null hvis personnr er null, utfør betaling")
+    public void testUtforBetaling_ikkeloggetinn() {
+
+        when(sjekk.loggetInn()).thenReturn(null);
+
+        List<Transaksjon> resultat = bankController.utforBetaling('int txID');
+
+        assertNull(resultat);
+    }
+
+    @Test
+    @DisplayName("Henter kundeinfo dersom kunde er logget inn")
+    public void hentKundeInfo_loggetInn() {
+
+        Kunde enKunde = new Kunde("01010110523",
+                "Lene", "Jensen", "Askerveien 22", "3270",
+                "Asker", "22224444", "HeiHei");
+
+        when(sjekk.loggetInn()).thenReturn("01010110523");
+
+        when(repository.hentKundeInfo(anyString())).thenReturn(enKunde);
+
+        Kunde resultat = bankController.hentKundeInfo();
+
+        assertEquals(enKunde, resultat);
+    }
+
+    @Test
+    @DisplayName("Returnerer null dersom kunde ikke er logget inn og man prøver å hente kundeinfo")
+    public void hentKundeInfo_IkkeloggetInn() {
+
+        when(sjekk.loggetInn()).thenReturn(null);
+
+        Kunde resultat = bankController.hentKundeInfo();
 
         assertNull(resultat);
     }
