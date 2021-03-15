091be1dc1abf6b8253f0bd6196bc232a832e5d38
Lokastico <56114098+Lokastico@users.noreply.github.com>
2021-03-15 17:56:47 +0100

    Måtte tydligvis committe. er visst bare noen tomme linjer forskjell, så det skal ikke ha noe å si.

--- src/main/java/oslomet/testing/API/AdminKontoControllerTest.java
+++ src/main/java/oslomet/testing/API/AdminKontoControllerTest.java
@@ -1,11 +1,29 @@
 package oslomet.testing.API;
+
 import org.testng.annotations.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import oslomet.testing.DAL.AdminRepository;
+import oslomet.testing.Models.Konto;
+import oslomet.testing.Sikkerhet.Sikkerhet;
 import static org.junit.Assert.*;
 
 public class AdminKontoControllerTest {
 
+    @Autowired
+    AdminRepository repository;
+
+    @Autowired
+    private Sikkerhet sjekk;
+
+
     @Test
-    public void testAdminKontoController(){
+    public void testHentAlleKonti(){
+
+
+    }
+
+    public void testRegistrerKonto(){
 
     }
 }
