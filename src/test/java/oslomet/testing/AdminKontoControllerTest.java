b553a55834c2ec202c1a9ef77b17b9759470e649
Lokastico <56114098+Lokastico@users.noreply.github.com>
2021-03-17 19:39:32 +0100

    importene er klare på adminKonto, prøvde meg på første testmetode.

--- src/test/java/oslomet/testing/AdminKontoControllerTest.java
+++ src/test/java/oslomet/testing/AdminKontoControllerTest.java
@@ -1,11 +1,45 @@
-package oslomet.testing.API;
+package oslomet.testing;
+import org.junit.jupiter.api.DisplayName;
 import org.testng.annotations.Test;
-import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import oslomet.testing.API.AdminKontoController;
+import oslomet.testing.DAL.AdminRepository;
+import oslomet.testing.Models.Konto;
+import oslomet.testing.Models.Kunde;
+import oslomet.testing.Models.Transaksjon;
+import oslomet.testing.Sikkerhet.Sikkerhet;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+
+@RunWith(MockitoJUnitRunner.class)
 public class AdminKontoControllerTest {
 
+    @InjectMocks
+    private AdminKontoController adminKontoController;
+
+    @Mock
+    private AdminRepository repository;
+
+    @Mock
+    private Sikkerhet sjekk;
+
     @Test
-    public void testAdminKontoController(){
+    @DisplayName("Henter alle konti dersom personnr ikke er null")
+    public void testHentAlleKonti_loggetInn(){
+        Konto enTransaksjon = new Konto("10101020123", "123456789", 1290,
+                "Lønnskonto", "NOK" , null);
+
 
     }
 }
