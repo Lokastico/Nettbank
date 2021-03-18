9b0d452075304f742327787645432ce93cade6b7
Lokastico <56114098+Lokastico@users.noreply.github.com>
2021-03-18 14:18:51 +0100

    importerte visst ikke alt til kundeControllertest, rettet opp det nå.

--- src/test/java/oslomet/testing/AdminKundeControllerTest.java
+++ src/test/java/oslomet/testing/AdminKundeControllerTest.java
@@ -10,6 +10,13 @@ import oslomet.testing.DAL.AdminRepository;
 import oslomet.testing.Models.Kunde;
 import oslomet.testing.Sikkerhet.Sikkerhet;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.Mockito.when;
+
 public class AdminKundeControllerTest {
 
     @Test
